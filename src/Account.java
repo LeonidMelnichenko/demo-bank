fda8cb7620d833c66cff4a0ff74ef7195bf286db
Leonid Melnichenko <melnichenko.leon@gmail.com>
2023-05-02 10:26:19 +0300

    refactor account class

--- src/Account.java
+++ src/Account.java
@@ -3,13 +3,15 @@ package src;
 public class Account {
     String name;
     String address;
+    String bank;
     double balance;
     public void display(){
-        System.out.println(name);
-        System.out.println(" (");
-        System.out.println(address);
-        System.out.println(") имеет на счету $");
-        System.out.println(balance);
+        System.out.print(name);
+        System.out.print(" (");
+        System.out.print(address);
+        System.out.print(") имеет на счету $");
+        System.out.print(balance);
+        System.out.println(bank);
     }
     public double getInterest(double percentageRate){
         return balance * percentageRate / 100.00;
