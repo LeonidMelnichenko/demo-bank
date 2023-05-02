3a7afb6f4fce697cc329d5d07458a4d7baa7bf66
Leonid Melnichenko <melnichenko.leon@gmail.com>
2023-05-02 10:28:54 +0300

    refactor useaccount class

--- src/UseAccount.java
+++ src/UseAccount.java
@@ -7,9 +7,11 @@ public class UseAccount {
         myAccount.name = "Барри";
         myAccount.address = "222 Inner Lane";
         myAccount.balance = 24.02;
+        myAccount.bank = "БПС-Банк";
         yourAccount.name = "Джейн";
         yourAccount.address = "121 Outer Street";
         yourAccount.balance = 55.63;
+        yourAccount.bank = "Приорбанк";
         myAccount.display();
         System.out.print(" плюс $");
         System.out.print(myAccount.getInterest(5.00));
